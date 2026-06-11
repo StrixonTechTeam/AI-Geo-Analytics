f49c68f98edde982ccd8a9ebd2582554e2b2dcfa
samstar001 <samstarmichael@gmail.com>
2026-06-11 20:26:46 +0100

    update: add requirements for project dependencies (#3)
    * update: add requirements for project dependencies
    
    * feat: initialize fastAPI with CORS and health check endpoint

---
+++ backend/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="AI-Geo-Analytics API") # creates API server application to handle the request
+
+# This allows Frontend React app (on a different port) to call the server
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], #allows frontend URL
+    allow_methods=["*"],# allows all http methods (PUT, GET, etc)
+    allow_headers=["*"], #allows any request header
+)
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
