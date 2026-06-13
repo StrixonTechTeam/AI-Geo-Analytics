d0a1c6996829703eb73f2b64bd9aa2fae10a54d9
samstar001 <samstarmichael@gmail.com>
2026-06-14 00:28:11 +0100

    feat: configure Neon database and create weather records table

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.database import engine, Base
+from app.models import weather_record  # import so Base knows about it
 
 app = FastAPI(title="AI-Geo-Analytics API") # creates API server application to handle the request
 
@@ -14,3 +16,11 @@ app.add_middleware(
 @app.get("/health") #creating an API endpoint/route for health
 def health_check():
     return {"status": "ok"}
+
+@app.on_event("startup") # tells the fastAPI to run the function below whent the application starts
+async def create_tables():
+    print("Creating tables....")
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all) # Run SQLAlchemy Metadata
+
+    print("Tables created!")
